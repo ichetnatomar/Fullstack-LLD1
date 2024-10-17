95259d6dbc7a290bbada85fb1a40b637bd208564
ichetnatomar <chetnatomar005@gmail.com>
2024-10-17 23:33:10 +0530

    push

---
+++ LLD1/Intro to JS/scriptNode.js
@@ -0,0 +1,54 @@
+// "use strict";
+console.log('hello from js');
+const radius = 5;
+const area = Math.floor(Math.PI * radius * radius * 100)/100;
+// const a = Math.floor(7853.9);
+
+// 3.14342566 * 10 * 10 = 31434.2566 = 314.34.0
+// console.log(area);
+// console.log(a);
+// x = 5;
+// console.log(x);
+
+// const prod = {
+//   name: "chetna",
+//   age: 34
+// };
+
+// console.log(prod.name+" is "+prod.age+" years old");
+
+// let arr = [];
+// arr.push('hello');
+// // console.log(arr);
+// let x = arr.pop();
+// // arr.pop();
+// console.log(arr);
+// // console.log(x);
+// arr.push('1');
+// arr.push(2);
+// console.log(arr);
+// // arr.forEach(element => {
+  
+// //   alert(element);
+
+// // } );
+
+// //add at start
+// arr.unshift(0);
+// console.log(arr);
+// let v = arr.shift();
+// console.log(v);
+// console.log(arr);
+// console.log(arr.indexOf(3));
+
+let arr = [0,1,2,3,4];
+// console.log(arr);
+// console.log(arr.splice(-200, 2));
+
+let str = "hello world";
+// let reversed = str.split('').reverse().join('');
+// let reversed = [...str].reverse().join('');
+// console.log(reversed);
+
+let reversed = str.split(' ');
+console.log(reversed);
